de5a5c3801feb507e34f32b3256d3eaad348a3cb
Jon Jagger <jon@jaggersoft.com>
2018-02-08 19:53:03 +0000

    fix incorrect image name in version script

--- check_version.sh
+++ check_version.sh
@@ -2,7 +2,7 @@
 set -e
 
 readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
-readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/bash sh -c 'chpl --version')
+readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/chapel sh -c 'chpl --version')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
