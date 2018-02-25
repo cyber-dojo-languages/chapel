1e6c35f5e0f2a208e5b7ec93c48916016d4fc941
Jon Jagger <jon@jaggersoft.com>
2018-02-25 17:40:34 +0000

    added hyperlink to readme version number

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![Build Status](https://travis-ci.org/cyber-dojo-languages/chapel.svg?branch=master)
 
-Version=1.16.0
+[Version=1.16.0](https://github.com/cyber-dojo-languages/chapel/blob/master/check_version.sh)
--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
+readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/chapel sh -c 'chpl --version')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
