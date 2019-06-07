4209bfed57ec2bc5d517b1e026c8a0bc656e5353
Jon Jagger <jon@jaggersoft.com>
2019-06-07 13:57:05 +0100

    fix check-version which relied in cwd

--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 [Version=1.18.0](https://github.com/cyber-dojo-languages/chapel/blob/master/check_version.sh)
 
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
-
--- check_version.sh
+++ check_version.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly EXPECTED=$(cat ${MY_DIR}/README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/chapel sh -c 'chpl --version')
 
-if echo ${ACTUAL} | grep -q ${EXPECTED}; then
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
 else
   echo "VERSION EXPECTED: ${EXPECTED}"
