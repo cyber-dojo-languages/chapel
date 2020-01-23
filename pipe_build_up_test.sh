ac25fd8d7ef19bb089cc1dc8a54bebe900010931
Jon Jagger <jon@jaggersoft.com>
2020-01-23 20:18:49 +0000

    Add context to circleci/config.yml

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,7 +1,28 @@
-#!/bin/bash
-set -e
+#!/bin/bash -Eeu
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly TMP_DIR="$(mktemp -d /tmp/cyber-dojo-image-builder.XXXXXXX)"
+remove_tmp_dir() { rm -rf "${TMP_DIR}" > /dev/null; }
+trap remove_tmp_dir INT EXIT
 
-${MY_DIR}/../image_builder/build_test_push_notify.sh
-${MY_DIR}/check_version.sh
+process_cdl_image_src_dir()
+{
+  local -r name=image_build_test_push_notify.sh
+  if [ -x "$(command -v ${name})" ]; then
+    >&2 echo "Found executable ${name} on the PATH"
+    echo "${name}"
+  else
+    local -r github=raw.githubusercontent.com
+    local -r org=cyber-dojo-tools
+    local -r repo=image_builder
+    local -r branch=master
+    local -r url="https://${github}/${org}/${repo}/${branch}/${name}"
+    >&2 echo "Did not find executable ${name} on the PATH"
+    >&2 echo "Attempting to curl it from ${url}"
+    curl --fail --output "${TMP_DIR}/${name}" --silent "${url}"
+    chmod 700 "${TMP_DIR}/${name}"
+    echo "${TMP_DIR}/${name}"
+  fi
+}
+
+"$(process_cdl_image_src_dir)" "${MY_DIR}"
