aafefd7359c5be73b7dd79eb80b59f08832cb3c4
Jon Jagger <jon@jaggersoft.com>
2020-02-02 10:34:43 +0000

    [ci skip] Move version-number into check_version.sh

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Chapel
 
-[Version=1.20.0](https://github.com/cyber-dojo-languages/chapel/blob/master/check_version.sh)
-
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
--- check_version.sh
+++ check_version.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-set -e
+#!/bin/bash -Eeu
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=$(cat ${MY_DIR}/README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
+readonly EXPECTED=1.20.0
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/chapel sh -c 'chpl --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
